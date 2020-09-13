d0a408865848215d4966c0bc2083186a03d7255a
Nikolay <dna2779@gmail.com>
2020-09-13 20:15:13 +0300

    Third trial

--- script.js
+++ script.js
@@ -26,7 +26,19 @@
 Проверить, чтобы все работало без ошибок в консоли */
 
 
-/*const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms ==null || isNaN(numberOfFilms)){
+           numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+         }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -34,21 +46,53 @@ const personalMovieDB = {
     actors: {},
     genres: [],
     privat: false
-};
+};  
 
+function remembarMyFilms(){
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Один из последних просмотренных фильмов?', ''),
+                  b = prompt('На сколько оцените его?', '');
+            
+            
+              if (a != null && b != null && a != '' && b != '' && a.length < 50 ) {
+                    personalMovieDB.movies[a] = b;
+                    console.log('done');
+                  } else {
+                console.log('error');
+                   i--;                //Вернутся на 1 повторение(итнрацию цикла назад)
+                  }
+                }
 
-for (let i = 0; i < 2; i++) {
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', '');
+    }
 
-      if (a != null && b != null && a != '' && b != '' && a.length < 50 ) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-      } else {
-    console.log('error');
-       i--;                  //Вернутся на 1 повторение(итнрацию цикла назад)
-      }
+    // remembarMyFilms();
+
+function detectPersonalLevel(){
+        const c = (personalMovieDB.count <= 10) ? 'Просмотренно довольно мало фильмов' :
+        (personalMovieDB.count >= 10 && personalMovieDB.count < 30) ? 'Вы классический зритель' :
+        (personalMovieDB.count >= 30) ? 'Вы киноман': 'Произошла ошибка';
+        console.log(c);
+
+    }
+    // detectPersonalLevel();
+ 
+    function showMyDB (hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    }
+    
+    showMyDB(personalMovieDB.privat);
+
+function writeYourGenres(){
+    for (let i = 1; i <=3; i++) {
+const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+personalMovieDB.genres[i - 1] = genre;
+    }
 }
+writeYourGenres();
+
+    /*
        if (personalMovieDB.count < 10) {
         console.log ('Просмотренно довольно мало фильмов');
        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
@@ -58,51 +102,34 @@ const a = prompt('Один из последних просмотренных ф
            } else {
             console.log('Произошла ошибка');
            }
-    
-
-
-
-console.log(personalMovieDB);
+           
 */
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-
-for (let i = 0; i < 2; i++) {
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', '');
-
-      if (a != null && b != null && a != '' && b != '' && a.length < 50 ) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-      } else {
-    console.log('error');
-       i--;                  //Вернутся на 1 повторение(итнрацию цикла назад)
-      }
+/*  
+const count = 9;
+ 
+switch (true) {
+    case count< 10:
+        console.log ('Просмотренно довольно мало фильмов');
+        break;
+    case count >= 10 && count < 30 :
+        console.log ('Вы классический зритель');
+        break;
+    case count >= 30 :
+        console.log ('Вы киноман');
+        break;
+    default :
+        console.log ('Произошла ошибка');
+ 
+ 
 }
-
-const c = (personalMovieDB.count <= 10) ? 'Просмотренно довольно мало фильмов' : 
-          (personalMovieDB.count >= 10 && personalMovieDB.count < 30) ? 'Вы классический зритель' :
-          (personalMovieDB.count >= 30) ? 'Вы киноман': 'Произошла ошибка';
-     
-
-console.log(c);
-console.log(personalMovieDB);
-
-
-
-
+*/
 
 
                                                                   //    Циклы
-/*let num = 50;
+/*
+
+let num = 50;
 while (num < 55) {
        console.log (num);
        num++;
@@ -119,4 +146,43 @@ for (let i = 1; i < 30; i++ ) {
 }
 console.log (i);
 
-}*/
+}
+
+*/
+//                                                 Функции
+//........................................................................................
+/*let num = 10;
+function showFirstMessage(text){
+     console.log(text);
+     num = 20;
+
+}
+
+showFirstMessage('Hello World!');
+console.log(num);
+*/
+
+/*
+function calc(a, b) {
+    return (a + b);
+}
+
+console.log(calc(1, 2));
+console.log(calc(1, 3));
+console.log(calc(1, 4));
+*/
+
+//...............................................................Методы
+/*
+const logg ='Hello world';
+// console.log(logg.slice(6));
+console.log(logg.substr(4, 6));     //Сколько символов вырезать(Начиная с 4-й 6 символов вырезать)
+
+const num = '12.2';
+console.log(Math.round(num));                       Если дробная часть числа больше, либо равна 0,5, 
+                                                     аргумент будет округлён до ближайшего большего  целого.
+const test = 12.2;
+console.log(parseFloat(test));                      Функция parseFloat() принимает строку в качестве аргумента
+                                                        и возвращает десятичное число (число с плавающей точкой)
+*/
+
